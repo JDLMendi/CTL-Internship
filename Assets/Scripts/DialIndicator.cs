7071d288389bc99c6aafc343a45a87d6ef6f9b5d
Jhundon <jhundon.mendi21@gmail.com>
2024-07-10 16:25:15 +0100

    Created Dial and Sliding Door

---
+++ Assets/Scripts/DialIndicator.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialIndicator : MonoBehaviour
+{
+    
+    public GameObject dial;
+
+    private Renderer indicatorRenderer;
+    private float dialValue = 0;
+
+    void Start()
+    {
+        indicatorRenderer = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        dialValue = dial.transform.rotation.eulerAngles.y;
+        Debug.Log("Dial Rotation: " + dialValue);
+
+        float normalisedRotation = dialValue/360f;
+        Color newColor = Color.Lerp(Color.white, Color.black, normalisedRotation);
+
+        indicatorRenderer.material.color = newColor;
+    }
+}
